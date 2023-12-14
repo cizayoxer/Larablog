3a4daf48b26e82dcf1ddc1b1565d2e8288cd4ec2
Romain Canevet <romaincanevet1@gmail.com>
2023-12-14 15:39:30 +0100

    Gestion de l'article

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <!-- Message flash -->
+            @if (session('success'))
+                <div class="bg-green-500 text-white p-4 rounded-lg mt-6 mb-6 text-center">
+                    {{ session('success') }}
+                </div>
+            @endif
+            @if (session('error'))
+                <div class="bg-red-500 text-white p-4 rounded-lg mt-6 mb-6 text-center">
+                    {{ session('error') }}
+                </div>
+            @endif
+                <!-- Articles -->
+                @foreach ($articles as $article)
+                    <div class="bg-white sm:rounded-lg mt-4">
+                        <div class="p-6 text-gray-900 ">
+                            <h2 class="text-2xl font-bold">{{ $article->title }}</h2>
+                            <p class="text-gray-700 ">{{ substr($article->content, 0, 30) }}...</p>
+                            <div class="text-right">
+                                <a href="{{ route('articles.edit', $article->id) }}" class="text-red-500 hover:text-red-700">🖊️</a>
+                            </div>
+                            <div class="text-right">
+                                <a href="{{ route('articles.delete', $article->id) }}" class="text-red-500 hover:text-red-700">🗑️</a>
+                            </div>
+                        </div>
+
+                    </div>
+                @endforeach
+
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/articles/create', [UserController::class, 'create'])->name('articles.create');
+    Route::post('/articles/store', [UserController::class, 'store'])->name('articles.store');
+    Route::get('/articles/{article}/edit', [UserController::class, 'edit'])->name('articles.edit');
+    Route::post('/articles/{article}/update', [UserController::class, 'update'])->name('articles.update');
+    Route::get('/articles/{article}/delete', [UserController::class, 'delete'])->name('articles.delete');
+});
+
+
+require __DIR__.'/auth.php';
