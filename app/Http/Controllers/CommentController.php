de69a6f049cb97d829789d0276ed2f8115e4f0d0
Romain Canevet <romaincanevet1@gmail.com>
2023-12-21 15:43:27 +0100

    Commentaires

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // Créateur de l'article (auteur)
+        if (Auth::check()){
+            Comment::create([
+                'article_id' => $request->input('article'),
+                'content' => $request->input('content'),
+                'user_id' => Auth::user()->id
+            ]);
+            return redirect()->back();
+        }
+        else{
+            return redirect()->route('login');
+        }
+
+
+    }
 }
--- resources/views/public/show.blade.php
+++ resources/views/public/show.blade.php
@@ -1,3 +1,4 @@
+
 <x-guest-layout>
     <div class="text-center">
         <h2 class="font-semibold text-xl text-gray-800 ">
@@ -13,5 +14,25 @@
         <div class="p-6 text-gray-900 ">
             <p class="text-gray-700 ">{{ $article->content }}</p>
         </div>
+    </div>
+    <div>
+
+
+    @foreach ($article->comments as $comment)
+        <div class="p-6 text-gray-900 bg-white shadow-md rounded-md mb-4">
+            <p class="text-2xl font-bold">{{ $comment->content }}</p>
+            <p class="text-1xl grayscale">{{ $comment->user->name }}</p>
+        </div>
+    @endforeach
+    @auth
+    <!-- Ajout d'un commentaire -->
+        <form action="{{ route('comments.store') }}" method="post" class="mt-6">
+            @csrf
+            <h1 class="text-xl font-bold mb-4">Commentaire</h1>
+            <input type="hidden" value="{{ $article->id }}" name="article">
+            <input type="text" name="content" class="p-2 border rounded-md w-full mb-4" placeholder="Votre commentaire">
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">Envoyer</button>
+        </form>
+    @endauth
     </div>
 </x-guest-layout>
